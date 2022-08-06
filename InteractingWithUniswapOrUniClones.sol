eda9796ac49d465e0a7273e669cac0456c61daf6
Pao Ge Vang <99309431+paogev123@users.noreply.github.com>
2022-08-05 20:32:19 -0700

    Initial commit

---
+++ InteractingWithUniswapOrUniClones.sol
@@ -0,0 +1,121 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IDEXVVS {
+  function getAmountsOut(uint amountIn, address[] memory path)
+    external
+    view
+    returns (uint[] memory amounts);
+
+  function swapExactTokensForTokens(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+
+  function swapExactTokensForETH(
+    uint amountIn,
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external returns (uint[] memory amounts);
+
+  function swapExactETHForTokens(
+    uint amountOutMin,
+    address[] calldata path,
+    address to,
+    uint deadline
+  ) external payable returns (uint[] memory amounts);
+
+  function addLiquidity(
+    address tokenA,
+    address tokenB,
+    uint amountADesired,
+    uint amountBDesired,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  )
+    external
+    returns (
+      uint amountA,
+      uint amountB,
+      uint liquidity
+    );
+
+  function removeLiquidity(
+    address tokenA,
+    address tokenB,
+    uint liquidity,
+    uint amountAMin,
+    uint amountBMin,
+    address to,
+    uint deadline
+  ) external returns (uint amountA, uint amountB);
+}
+
+contract TestUniswap {
+  address private constant router = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
+  address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+
+  function swap(
+    address _tokenIn,
+    address _tokenOut,
+    uint _amountIn,
+    uint _amountOutMin,
+    address _to
+  ) external {
+    IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
+    IERC20(_tokenIn).approve(router, _amountIn);
+
+    address[] memory path;
+    if (_tokenIn == WETH || _tokenOut == WETH) {
+      path = new address[](2);
+      path[0] = _tokenIn;
+      path[1] = _tokenOut;
+    } else {
+      path = new address[](3);
+      path[0] = _tokenIn;
+      path[1] = WETH;
+      path[2] = _tokenOut;
+    }
+
+    IDEXVVS(router).swapExactTokensForTokens(
+      _amountIn,
+      _amountOutMin,
+      path,
+      _to,
+      block.timestamp
+    );
+  }
+
+  function getAmountOutMin(
+    address _tokenIn,
+    address _tokenOut,
+    uint _amountIn
+  ) external view returns (uint) {
+    address[] memory path;
+    if (_tokenIn == WETH || _tokenOut == WETH) {
+      path = new address[](2);
+      path[0] = _tokenIn;
+      path[1] = _tokenOut;
+    } else {
+      path = new address[](3);
+      path[0] = _tokenIn;
+      path[1] = WETH;
+      path[2] = _tokenOut;
+    }
+
+    // same length as path
+    uint[] memory amountOutMins =
+      IDEXVVS(router).getAmountsOut(_amountIn, path);
+
+    return amountOutMins[path.length - 1];
+  }
+}
